4360ed3c5d7c06c4209426a1940e3d238ec84dad
NandaCj <nandha.paramasivam@gmail.com>
2020-05-03 15:29:45 +0530

    api for to get adaptive card templates "get_card_templates" added docstring for existing classes in resources.py modified imports in api.py added utils.py to have utils for api

--- app/resources.py
+++ app/resources.py
@@ -1,16 +1,14 @@
+""" resources for api """
 import base64
 import io
 import json
 import logging
 import os
-import sys
-from datetime import datetime as dt
-
 from PIL import Image
-from flask import request
+from flask import request, jsonify
 from flask_restplus import Resource
-
 from mystique.utils.predict_card import PredictCard
+from utils import get_templates
 
 
 logger = logging.getLogger('mysitque')
@@ -22,11 +20,30 @@ label_path = os.path.join(os.path.dirname(__file__), "../mystique/training/objec
 
 
 class PredictJson(Resource):
+    """
+    Handling Adaptive Card Predictions
+    """
 
     def post(self):
-
+        """
+        predicts the adaptive card json for the posted image
+        :return: adaptive card json
+        """
         imgdata = base64.b64decode(request.json.get('image', ''))
         image = Image.open(io.BytesIO(imgdata))
 
         return_json = PredictCard().main(image=image)
         return json.loads(return_json)
+
+class GetCardTemplates(Resource):
+    """
+    Handling adaptive card template images
+    """
+
+    def get(self):
+        """
+        returns adaptive card templates images
+        :return: adaptive card templates images in str format
+        """
+        templates = get_templates()
+        return templates
